74f9b95bfc4b94f48775a8c867b37ea0b4a32130
Patrik Westlund <patrik@patrikwestlund.se>
2012-12-03 14:25:44 +0100

    Use public http instead of git ssh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clone git project and cd into the folder.
-git clone git@github.com:PatrikWestlund/dotfiles.git ~/.dotfiles
+git clone https://github.com/PatrikWestlund/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 
 # Go trough all the files available in this folder.
