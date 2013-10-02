ac635db6f198ef53dc524944fe17c0ef638b79c0
Patrik Westlund <patrik@patrikwestlund.se>
2013-10-02 16:40:34 +0200

    Modify FISH prompt

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,36 @@
+# name: Suvash
+
+set -xg fish_color_user magenta
+set -xg fish_color_host yellow
+
+function fish_prompt --description 'Write out the prompt'
+  set -l last_status $status
+
+  # User
+  set_color $fish_color_user
+  echo -n (whoami)
+  set_color normal
+
+  echo -n '@'
+
+  # Host
+  set_color $fish_color_host
+  echo -n (hostname -s)
+  set_color normal
+
+  echo -n ':'
+
+  # PWD
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+
+  # Nextline
+  echo
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
+
+  echo -n '> '
+  set_color normal
+end
---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,4 @@
+function fish_right_prompt -d "Write out right prompt"
+  __git_prompt
+  echo
+end
