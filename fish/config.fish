d92d306caf1f76cdc7d1bfd9eaffae9946fd5a44
Patrik Westlund <patrik@patrikwestlund.se>
2013-10-02 16:41:34 +0200

    Update FISH config

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,9 @@ set -x PATH /usr/local/bin /usr/local/sbin /usr/local/share/npm/bin $PATH
 
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
+
+set -l plugins_path (dirname (status -f))/plugins
+
+fish_aliases
+
+. $plugins_path/z.fish
