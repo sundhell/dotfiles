e2e9cb340fe395c4b88334fd5255dcc1ef110539
Patrik Westlund <patrik@patrikwestlund.se>
2013-10-20 22:27:44 +0200

    Add support for rbenv for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,11 @@
 set -x PATH /usr/local/bin /usr/local/sbin /usr/local/share/npm/bin $PATH
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
 
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
+
+rbenv rehash >/dev/null ^&1
 
 set -l plugins_path (dirname (status -f))/plugins
 
