ad284487cd16f477992820224fb0f7444ed5378b
Patrik Westlund <patrik@patrikwestlund.se>
2014-08-27 16:11:51 +0200

    Fix PATH in fish

--- fish/functions/fish_env_variables.fish
+++ fish/functions/fish_env_variables.fish
@@ -1,9 +1,8 @@
 function fish_env_variables -d "Set env variables"
-  set PATH $HOME/.rbenv/bin $PATH
+  set PATH $HOME/bin $PATH
   set PATH $HOME/.rbenv/shims $PATH
   set PATH $HOME/.pyenv/shims $PATH
-
-  set -x PATH $PATH /usr/local/bin /usr/local/sbin /usr/local/share/npm/bin
+  set PATH /usr/local/share/npm/bin $PATH
 
   set -x LANG en_US.UTF-8
   set -x LC_ALL en_US.UTF-8
