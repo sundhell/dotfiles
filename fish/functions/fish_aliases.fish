128bf8a0dab3c8e026a29ac4e299267b0f233ce8
Patrik Westlund <patrik@patrikwestlund.se>
2014-09-17 10:46:08 +0200

    Alias gc to git commit -v

--- fish/functions/fish_aliases.fish
+++ fish/functions/fish_aliases.fish
@@ -2,7 +2,7 @@ function fish_aliases -d "Store aliases"
   alias gst "git status"
   alias gco "git checkout"
   alias gb "git branch"
-  alias gc "git commit"
+  alias gc "git commit -v"
 
   alias be "bundle exec"
   alias prst "touch tmp/restart.txt"
