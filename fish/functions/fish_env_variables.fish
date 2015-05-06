371431977a1407bbaa6ba598e60e74e155a530f2
Patrik Sundhäll <patrik@sundhall.se>
2015-05-06 14:29:40 +0200

    Fish: Actually export the env variables

--- fish/functions/fish_env_variables.fish
+++ fish/functions/fish_env_variables.fish
@@ -6,8 +6,8 @@ function fish_env_variables -d "Set env variables"
   set PATH $HOME/.pyenv/shims $PATH
   set PATH /usr/local/share/npm/bin $PATH
 
-  set -x LANG 'en_US.UTF-8'
-  set -x LC_ALL 'en_US.UTF-8'
-  set -x LC_CTYPE 'en_US.UTF-8'
-  set -x EDITOR 'vim'
+  set -xg LANG 'en_US.UTF-8'
+  set -xg LC_ALL 'en_US.UTF-8'
+  set -xg LC_CTYPE 'en_US.UTF-8'
+  set -xg EDITOR 'vim'
 end
