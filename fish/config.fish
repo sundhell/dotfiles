cd2d680a102f7c6b56b104647baf77eb4342469e
Patrik Sundhäll <patrik@sundhall.se>
2015-07-27 00:09:23 +0200

    Fish: Use fish_user_paths instead of PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,25 @@
 set -g -x fish_greeting ''
 
+# Set up PATH
+set -g fish_user_paths $HOME/bin
+
+if which brew >/dev/null ^&1
+  set -g fish_user_paths /usr/local/bin
+  set -g fish_user_paths /usr/local/sbin
+end
+
+if which rbenv >/dev/null ^&1
+  set -g fish_user_paths $HOME/.rbenv/shims
+end
+
+if which pyenv >/dev/null ^&1
+  set -g fish_user_paths $HOME/.pyenv/shims
+end
+
+if which composer >/dev/null ^&1
+  set -g fish_user_paths $HOME/.composer/vendor/bin
+end
+
 fish_env_variables
 fish_user_abbreviations
 fish_vi_mode
--- fish/functions/fish_env_variables.fish
+++ fish/functions/fish_env_variables.fish
@@ -1,11 +1,4 @@
 function fish_env_variables -d "Set env variables"
-  set PATH $HOME/bin $PATH
-  set PATH /usr/local/bin $PATH
-  set PATH /usr/local/sbin $PATH
-  set PATH $HOME/.rbenv/shims $PATH
-  set PATH $HOME/.pyenv/shims $PATH
-  set PATH /usr/local/share/npm/bin $PATH
-
   set -xg LANG 'en_US.UTF-8'
   set -xg LC_ALL 'en_US.UTF-8'
   set -xg LC_CTYPE 'en_US.UTF-8'
