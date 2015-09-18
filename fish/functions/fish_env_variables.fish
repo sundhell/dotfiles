28f0d81d8f44d8787667eaf7b7755faba2b5962e
Patrik Sundhäll <patrik@sundhall.se>
2015-09-18 08:52:20 +0200

    FISH: Add colors to manual output

--- fish/functions/fish_env_variables.fish
+++ fish/functions/fish_env_variables.fish
@@ -4,4 +4,12 @@ function fish_env_variables -d "Set env variables"
   set -xg LC_CTYPE 'en_US.UTF-8'
   set -xg EDITOR 'vim'
   set -xg PAGER 'less'
+
+  set -xg LESS_TERMCAP_mb (printf "\e[01;31m")
+  set -xg LESS_TERMCAP_md (printf "\e[01;31m")
+  set -xg LESS_TERMCAP_me (printf "\e[0m")
+  set -xg LESS_TERMCAP_se (printf "\e[0m")
+  set -xg LESS_TERMCAP_so (printf "\e[01;44;33m")
+  set -xg LESS_TERMCAP_ue (printf "\e[0m")
+  set -xg LESS_TERMCAP_us (printf "\e[01;32m")
 end
