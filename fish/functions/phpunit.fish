ec0d1120c2df9428ce3404fbb3619fb51ac125c1
Patrik Sundhäll <patrik@sundhall.se>
2015-10-14 16:55:21 +0200

    Fish: Make phpunit work with more projects

--- fish/functions/phpunit.fish
+++ fish/functions/phpunit.fish
@@ -1,7 +1,7 @@
 function phpunit
 	if test -f $PWD/../../Vagrantfile
 		echo 'Running phpunit inside vagrant...'
-		vagrant ssh -c "cd /vagrant/system; phpunit $argv"
+		vagrant ssh -c "cd /vagrant/"(basename $PWD)"; phpunit $argv"
 	else
 		command phpunit $argv
 	end
