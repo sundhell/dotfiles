0cd43863284bed98dffce01c24bbc1acacbe547f
Patrik Sundhäll <patrik@sundhall.se>
2015-12-11 12:04:21 +0100

    Fish: Add env var to support changing cursor in neovim

--- fish/functions/fish_env_variables.fish
+++ fish/functions/fish_env_variables.fish
@@ -4,6 +4,7 @@ function fish_env_variables -d "Set env variables"
   set -xg LC_CTYPE 'en_US.UTF-8'
   set -xg EDITOR 'vim'
   set -xg PAGER 'less'
+  set -xg NVIM_TUI_ENABLE_CURSOR_SHAPE '1'
 
   set -xg LESS_TERMCAP_mb (printf "\e[01;31m")
   set -xg LESS_TERMCAP_md (printf "\e[01;31m")
