17752296785db6e0da000cc3a14e307aa3c6a4cb
Patrik Sundhäll <patrik@sundhall.se>
2016-06-28 14:41:09 +0200

    Fish: No need for special docker logic

--- fish/functions/docker-compose.fish
+++
@@ -1,8 +0,0 @@
-function docker-compose
-	if which docker-machine >/dev/null ^&1
-		eval (docker-machine env default)
-		command docker-compose $argv
-	else
-		command docker-compose $argv
-	end
-end
--- fish/functions/docker.fish
+++ fish/functions/docker.fish
@@ -3,11 +3,6 @@ function docker
 		echo 'Running docker inside vagrant...'
 		vagrant ssh -c "cd /vagrant; docker $argv"
 	else
-		if which docker-machine >/dev/null ^&1
-			eval (docker-machine env default)
-			command docker $argv
-		else
-			command docker $argv
-		end
+		command docker $argv
 	end
 end
