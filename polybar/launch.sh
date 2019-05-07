1a184d9c53444fe4dc4d71866364d216b1ed36ee
Patrik Sundhäll <patrik@sundhall.se>
2019-05-07 09:29:17 +0200

    Update old config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar bar & done
+# Launch polybar on every connected monitor
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar top & done
 
 echo "Bars launched..."
