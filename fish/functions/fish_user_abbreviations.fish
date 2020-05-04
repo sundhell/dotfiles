40f8400727de36f7e3e13be42ca78f84c73f3c07
Patrik Sundhäll <patrik@sundhall.se>
2020-05-04 10:18:38 +0200

    FISH: Update abbreviations format

--- fish/functions/fish_user_abbreviations.fish
+++ fish/functions/fish_user_abbreviations.fish
@@ -1,30 +1,30 @@
 function fish_user_abbreviations
   set -U fish_user_abbreviations
-    abbr -a l='ls -a'
-    abbr -a ll='ls -lah'
-    abbr -a lsd='tree --dirsfirst -ChF -L 1'
+    abbr -a l 'ls -a'
+    abbr -a ll 'ls -lah'
+    abbr -a lsd 'tree --dirsfirst -ChF -L 1'
 
-    abbr -a ta='tmux attach'
-    abbr -a tl='tmux list-sessions'
+    abbr -a ta 'tmux attach'
+    abbr -a tl 'tmux list-sessions'
 
-    abbr -a ga='git add'
-    abbr -a gst='git status -sb'
-    abbr -a gd='git diff'
-    abbr -a gc='git commit -v'
-    abbr -a gco='git checkout'
-    abbr -a gb='git branch'
-    abbr -a gl='git log'
+    abbr -a ga 'git add'
+    abbr -a gst 'git status -sb'
+    abbr -a gd 'git diff'
+    abbr -a gc 'git commit -v'
+    abbr -a gco 'git checkout'
+    abbr -a gb 'git branch'
+    abbr -a gl 'git log'
 
-    abbr -a be='bundle exec'
-    abbr -a rc='rails console'
+    abbr -a be 'bundle exec'
+    abbr -a rc 'rails console'
 
-    abbr -a prst='touch tmp/restart.txt'
+    abbr -a prst 'touch tmp/restart.txt'
 
-    abbr -a rflush='redis-cli flushall'
-    abbr -a mflush='echo"flush_all" | nc localhost 11211'
+    abbr -a rflush 'redis-cli flushall'
+    abbr -a mflush 'echo"flush_all" | nc localhost 11211'
 
-    abbr -a bu='brew update; brew upgrade; brew cleanup'
+    abbr -a bu 'brew update; brew upgrade; brew cleanup'
 
-    abbr -a vha='vagrant global-status | awk "/running/{print $1}" | xargs -n 1 -- vagrant halt'
-    abbr -a vsa='vagrant global-status | awk "/running/{print $1}" | xargs -n 1 -- vagrant up'
+    abbr -a vha 'vagrant global-status | awk "/running/{print $1}" | xargs -n 1 -- vagrant halt'
+    abbr -a vsa 'vagrant global-status | awk "/running/{print $1}" | xargs -n 1 -- vagrant up'
 end
